b16abeffe6ec04158cc1a64a3126a115f6e7d3ab
Almas khan <akkak@hh>
2017-02-27 10:23:43 -0500

    Complete NPuzzle

---
+++ NPuzzle/GameForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace NPuzzle
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtColumns = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.displayPanel = new System.Windows.Forms.Panel();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(471, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Rows :";
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(132, 46);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(100, 20);
+            this.txtRows.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Columns : ";
+            // 
+            // txtColumns
+            // 
+            this.txtColumns.Location = new System.Drawing.Point(132, 78);
+            this.txtColumns.Name = "txtColumns";
+            this.txtColumns.Size = new System.Drawing.Size(100, 20);
+            this.txtColumns.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnGenerate.Location = new System.Drawing.Point(294, 60);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(91, 35);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // displayPanel
+            // 
+            this.displayPanel.AutoSize = true;
+            this.displayPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.displayPanel.Location = new System.Drawing.Point(15, 120);
+            this.displayPanel.Name = "displayPanel";
+            this.displayPanel.Size = new System.Drawing.Size(442, 321);
+            this.displayPanel.TabIndex = 6;
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "dlgOpen";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 453);
+            this.Controls.Add(this.displayPanel);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtColumns);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GameForm";
+            this.Text = "NPuzzle";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtColumns;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Panel displayPanel;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+    }
+}
+
---
+++ NPuzzle/GameForm.cs
@@ -0,0 +1,450 @@
+﻿/* 
+ * NPuzzle
+ * Revision History
+ *      Almas Khan: 2016.10.25: Created
+ *      Almas Khan: 2016.10.28: coded
+ *      Almas Khan: 2016.10.28: Debugged
+ *      
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NPuzzle
+{
+    /// <summary>
+    /// A class that shows NPuzzle Game
+    /// </summary>
+    public partial class GameForm : Form
+    {
+        //Constants declaration
+        const int tileHeight = 50;
+        const int tileWidth = 50;
+        //Global variables
+        int rows;
+        int columns;
+        int counter = 0;
+        int arraySize;
+        int[,] tiles;
+        Tile free;
+        int startX = 50;
+        int startY = 50;
+        int[] random;
+        Tile tile;
+
+        /// <summary>
+        /// The constructor of the class
+        /// </summary>
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event handler will generate the puzzle according to the input of rows and columns
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //Method called to clear the panel remove all buttons and set counter to zero
+            clearPanel();
+            try
+            {
+                rows = Convert.ToInt32(txtRows.Text);
+                columns = Convert.ToInt32(txtColumns.Text);
+                if (rows.ToString() == "" && columns.ToString() == "")
+                {
+                    throw new Exception("Please enter row and column value to generate game");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " +ex.Message);
+            }
+            tiles = new int[rows, columns];
+            arraySize = tiles.Length;
+            random = new int[arraySize];
+            //Method called to generate random numbers
+            random = generateUniqueRandom(1, arraySize);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    tile = new Tile();
+                    tile.Left = startX;
+                    tile.Top = startY;
+                    tile.Height = tileHeight;
+                    tile.Width = tileWidth;
+                    tile.Font = new Font("Georgia", 16);
+                    tile.BackColor = Color.White;
+                    if (random[counter] == arraySize)
+                    {
+                        tile.Visible = false;
+                        tiles[i, j] = 0;
+                        tile.Text = 0.ToString();
+                        free = tile;
+                    }
+                   else
+                   {
+                        tile.Text = random[counter].ToString();
+                        tiles[i, j] = random[counter];
+                    }
+                    displayPanel.Controls.Add(tile);
+                    tile.Click += new EventHandler(tileClickEvent);
+
+                    startX += tileWidth;
+                    counter++;
+                }
+                startX = tileWidth;
+                startY += tileHeight;
+            }
+        }
+
+        /// <summary>
+        /// Event handler called when tile is clicked, check its text if text is something then other methods will be called
+        /// to swap clicked til with the free tile and will check game end
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tileClickEvent(object sender, EventArgs e)
+        {
+            Tile clickButton = sender as Tile;
+            if (clickButton.Text != "")
+            {
+                Tile tile = (Tile)sender;
+                swapTiles(tile);
+                checkGameEnd();
+            }
+        }
+        /// <summary>
+        /// Method is called to check game end condition. Current positions of tiles will compare with the exact positions
+        /// If it matches then game ends and panel will be cleared also textboxes will be cleared
+        /// </summary>
+        private void checkGameEnd()
+        {
+            string correctPositions = "";
+            string currentPositions = "";
+            int value = 1;
+            int[,] correctArray = new int[rows,columns];
+            arraySize = rows * columns;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    currentPositions += tiles[i, j];
+                    if (value == arraySize)
+                    {
+                        correctArray[i, j] = 0;
+                        correctPositions += correctArray[i, j];
+                    }
+                    else
+                    {
+                        correctArray[i, j] = value;
+                        correctPositions += correctArray[i, j];
+                    }
+                    value++;
+                }
+            }
+            
+            if (currentPositions == correctPositions)
+            {
+                MessageBox.Show("Game Ends!!");
+                clearPanel();
+                txtColumns.Text = "";
+                txtRows.Text = "";
+            }
+          }
+
+        /// <summary>
+        /// Method will swap the positions of clicked tile with the free tile
+        /// </summary>
+        /// <param name="tile">Selected tile</param>
+        private void swapTiles(Tile tile)
+        {
+            const int startIndex = 2;
+            int tempLeft;
+            int tempTop;
+            string i;
+            string j;
+            int tileRow;
+            int tileColumn;
+            int freeRow;
+            int freeColumn;
+
+            if ((tile.Left == free.Left - tileWidth || tile.Left == free.Left + tileWidth)
+                && (tile.Top == free.Top)
+                || (tile.Top == free.Top - tileHeight || tile.Top == free.Top + tileHeight)
+                && (tile.Left == free.Left))
+            {
+                tempLeft = tile.Left;
+                tempTop = tile.Top;
+                tile.Left = free.Left;
+                tile.Top = free.Top;
+                free.Left = tempLeft;
+                free.Top = tempTop;
+
+                i = indexGetter(Convert.ToInt32(tile.Text));
+                tileRow = Convert.ToInt32(i.Substring(0, 1));
+                tileColumn = Convert.ToInt32(i.Substring(startIndex, 1));
+                j = indexGetter(Convert.ToInt32(free.Text));
+                freeRow = Convert.ToInt32(j.Substring(0, 1));
+                freeColumn = Convert.ToInt32(j.Substring(startIndex, 1));
+                positionSwap(ref tiles[tileRow, tileColumn], ref tiles[freeRow, freeColumn]);
+            }
+        }
+        /// <summary>
+        /// Methos will update the array according to swapped tiles
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void positionSwap(ref int x, ref int y)
+        {
+            int tempVar = x;
+            x = y;
+            y = tempVar;
+        }
+
+        /// <summary>
+        /// Method will find index of the clicked tile
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private string indexGetter(int x)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                { 
+                    if (tiles[i,j] == x)
+                    {
+                        return i+","+j;
+                    }
+                }
+            }
+            return "empty";
+        }
+
+        /// <summary>
+        /// Method will generate unique random numbers to assign the puzzles with those generated numbers
+        /// </summary>
+        /// <param name="min">Min number that is 1</param>
+        /// <param name="max">Max number that is size of the array</param>
+        /// <returns></returns>
+        private int[] generateUniqueRandom(int min, int max)
+        {
+            int n = max - min + 1;
+            int[] result = new int[n];
+            List<int> tiles = new List<int>();
+
+            for (int i = min; i <= max; i++)
+            {
+                tiles.Add(i);
+            }
+            Random r = new Random();
+            int j = 0;
+            while (tiles.Count > 0)
+            {
+                int index = r.Next(tiles.Count); 
+                result[j++] = tiles[index];
+                tiles.RemoveAt(index);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Method will remove all the controls from the panel and set counter to zero
+        /// </summary>
+        private void clearPanel()
+        {
+            counter = 0;
+            startX = tileHeight;
+            startY = tileWidth;
+            displayPanel.Controls.Clear();
+        }
+
+        /// <summary>
+        /// Method will be called to read all the numbers from the loaded file and will generate tiles according to that
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        private void doLoad(string filename)
+        {
+            int value;
+            clearPanel();
+            StreamReader reader = new StreamReader(filename);
+            rows = Convert.ToInt32(reader.ReadLine());
+            columns = Convert.ToInt32(reader.ReadLine());
+            txtRows.Text = rows.ToString();
+            txtColumns.Text = columns.ToString();
+            tiles = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    tile = new Tile();
+                    tile.Left = startX;
+                    tile.Top = startY;
+                    tile.Height = tileHeight;
+                    tile.Width = tileWidth;
+                    tile.Font = new Font("Georgia", 16);
+                    tile.BackColor = Color.White;
+                    value = Convert.ToInt32(reader.ReadLine());
+                    if (value == 0)
+                    {
+                        tile.Visible = false;
+                        tiles[i, j] = 0;
+                        tile.Text = 0.ToString();
+                        free = tile;
+                    }
+                    else
+                    {
+                        tile.Text = value.ToString();
+                        tiles[i, j] = value;
+                    }
+                    displayPanel.Controls.Add(tile);
+                    tile.Click += new EventHandler(tileClickEvent);
+                    startX += tileHeight;
+                    counter++;
+                }
+                startX = tileWidth;
+                startY += tileWidth;
+            }
+            reader.Close();          
+        }
+        /// <summary>
+        /// Event handler will be called to get exit fron the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Method will write all values from the tiles array to the file
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <param name="rowValue">Number of rows</param>
+        /// <param name="colValue">Number of columns</param>
+        private void doSave(string filename, int rowValue, int colValue)
+        {
+
+            StreamWriter writer = new StreamWriter(filename);
+            writer.WriteLine(rowValue);
+            writer.WriteLine(colValue);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    writer.WriteLine(tiles[i, j].ToString());
+                }
+            }
+            writer.Close();
+        }
+        /// <summary>
+        /// Method will save the current positions of the tiles based on their text in the text file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int rowValue = Convert.ToInt32(txtRows.Text);
+            int colValue = Convert.ToInt32(txtColumns.Text);
+            DialogResult result = dlgSave.ShowDialog();
+            dlgSave.InitialDirectory = @"C:\";
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                case DialogResult.Ignore:
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.None:
+                    break;
+                case DialogResult.OK:
+                    try
+                    {
+                        string filename = dlgSave.FileName;
+                        doSave(filename, rowValue,colValue);
+                        MessageBox.Show("Your file is saved");
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Error in file save");
+                    }
+
+                    break;
+                case DialogResult.Retry:
+                    break;
+                case DialogResult.Yes:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Method will load the selected file and call doLoad method to make tiles
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgOpen.ShowDialog();
+            dlgOpen.InitialDirectory = @"C:\";
+            switch (result)
+            {
+                case DialogResult.Abort:
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                case DialogResult.Ignore:
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.None:
+                    break;
+                case DialogResult.OK:
+                    try
+                    {
+                        string filename = dlgOpen.FileName;
+                        doLoad(filename);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show("Error in file load"+ ex.Message);
+                    }
+                    break;
+                case DialogResult.Retry:
+                    break;
+                case DialogResult.Yes:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+    
+
